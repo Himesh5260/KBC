295f78ebbef35538ccfbddcad54cecad6bd553ef
HIMESH MEHTA <himeshmehta07@gmail.com>
2026-04-08 01:03:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -193,4 +193,4 @@ def result():
                            amount=format_inr(amount), amount_raw=amount)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
